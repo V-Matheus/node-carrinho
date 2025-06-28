4a5b69801418936c540f592ca89a92ca023f3af9
Victor Matheus <victormatheus507@gmail.com>
2025-06-28 09:58:32 -0300

    logica do item

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
+import createItem from './services/item.js';
 const carrinho = []
 
+const item1 = await createItem('Camiseta', 29.90, 2);
+const item2 = await createItem('Calça', 89.90, 3);
+
+console.log(item2.subtotal());
--- src/services/item.js
+++ src/services/item.js
@@ -1,2 +1,8 @@
-async function createItem(name, price, quantity) {}
-
+export default async function createItem(name, price, quantity) {
+  return {
+    name,
+    price,
+    quantity,
+    subtotal: () => price * quantity,
+  };
+}
